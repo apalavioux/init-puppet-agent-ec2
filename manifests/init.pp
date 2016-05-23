8b835a4353060e5e360afd61458d745d8a76acf7
antoine palavioux <apalavioux@smartwavesa.com>
2016-05-23 15:18:54 +0200

    first draft

---
+++ manifests/init.pp
@@ -0,0 +1,32 @@
+Exec {
+		path => ["/usr/bin","/bin","/usr/sbin","/sbin"]
+}
+
+exec { 	"hostname":
+		command => "hostnamectl set-hostname puppet-agent-1",
+}
+
+$userdata = parse_userdata()
+
+exec { 	"userdata":
+		command => "echo $userdata",
+}
+
+    if has_key($userdata, 'hostname') {
+        $hostname = $userdata['hostname']
+    } else {
+        warning('Unable to parse hostname from userdata')
+        $hostname = false
+    }
+    if has_key($userdata, 'puppet') and has_key($userdata['puppet'], 'server') {
+        $puppet_server = $userdata['puppet']['server']
+    } else {
+        $puppet_server = false
+    }
+
+exec { 	"hostname":
+		command => "echo $hostname",
+}
+exec { 	"puppet_server":
+		command => "echo $puppet_server",
+}
