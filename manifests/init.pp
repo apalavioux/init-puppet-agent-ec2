2e0453c750485e7b1ed101c6f83131e46b74e3dc
antoine palavioux <apalavioux@smartwavesa.com>
2016-05-23 16:35:56 +0200

    no message

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,16 +8,16 @@ $userdata = parse_userdata()
 notify {"userdata: ${userdata}":}
 
     if has_key($userdata, 'hostname') {
-        $hostname = $userdata['hostname']
+        $host_name = $userdata['hostname']
     }
     if has_key($userdata, 'puppet') and has_key($userdata['puppet'], 'server') {
         $puppet_server = $userdata['puppet']['server']
     }
 
-notify {"hostname: ${hostname}":}
+notify {"hostname: ${host_name}":}
 notify {"puppet_server: ${puppet_server}":}
 
 
 exec { 	"host":
-		command => "hostnamectl set-hostname ${hostname}",
+		command => "hostnamectl set-hostname ${host_name}",
 }
