abdb2371cec356429e74e2e2546f5615e1964439
antoine palavioux <apalavioux@smartwavesa.com>
2016-06-08 11:51:45 +0200

    bad naming

--- setup_puppet_agent.sh
+++ setup_puppet_agent.sh
@@ -4,5 +4,5 @@ sudo yum -y install puppet-agent
 sudo ln -s /opt/puppetlabs/puppet/bin/puppet /usr/bin/puppet
 sudo yum -y install git
 (cd /opt/puppetlabs/puppet/modules/ && sudo git clone https://github.com/apalavioux/set_hostname_ec2.git)
-sudo cp /opt/puppetlabs/puppet/modules/set_hostname_ec2/puppetagentsetup.sh /var/lib/cloud/scripts/per-boot/
-sudo chmod +x /var/lib/cloud/scripts/per-boot/puppetagentsetup.sh
+sudo cp /opt/puppetlabs/puppet/modules/set_hostname_ec2/launch_hostname_check.sh /var/lib/cloud/scripts/per-boot/
+sudo chmod +x /var/lib/cloud/scripts/per-boot/launch_hostname_check.sh
