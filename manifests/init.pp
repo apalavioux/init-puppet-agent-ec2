2c9aa7da9258a4afd9a71bef5c1f9371e2794a3c
antoine palavioux <apalavioux@smartwavesa.com>
2016-06-15 09:41:02 +0200

    no message

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,10 +10,14 @@ class set_hostname_ec2 {
 
   if has_key($userdata, 'hostname') {
     $host_name = $userdata['hostname']
+  }elsif $::ec2_tag_hostname {
+    $host_name = $::ec2_tag_hostname
   }
 
   if has_key($userdata, 'domainname') {
     $domain_name = $userdata['domainname']
+  }elsif $::ec2_tag_domain {
+    $domain_name = $::ec2_tag_hostname
   }
 
   $metadata = parse_metadata()
@@ -21,10 +25,44 @@ class set_hostname_ec2 {
 
   if has_key($userdata, 'puppet') and has_key($userdata['puppet'], 'server') {
     $puppet_server = $userdata['puppet']['server']
+  }elsif $::ec2_tag_puppet_server {
+    $puppet_server = $::ec2_tag_puppet_server
   }
 
-  exec {   'host':
-    command => "hostnamectl set-hostname ${host_name}.${domain_name}",
+  if has_key($userdata, 'puppet') and has_key($userdata['puppet'], 'environment') {
+    $puppet_env = $userdata['puppet']['environment']
+  }elsif $::ec2_tag_puppet_env {
+    $puppet_env = $::ec2_tag_puppet_env
+  }
+  
+  $puppet_runinterval = '10m'
+  if $::ec2_tag_puppet_runinterval {
+    $puppet_runinterval = $::ec2_tag_puppet_runinterval
+  }
+
+  if $host_name and $domain_name {
+    exec {   'host':
+      command => "hostnamectl set-hostname ${host_name}.${domain_name}",
+    }
+    file { '/etc/sysconfig/network':
+        ensure  => file,
+        path    => '/etc/sysconfig/network',
+        content => template('set_hostname_ec2/network.erb'),
+        owner   => root,
+        group   => root,
+        mode    => '0644',
+      }
+      file { '/etc/hosts':
+          ensure  => file,
+          path    => '/etc/hosts',
+          content => template('set_hostname_ec2/hosts.erb'),
+          owner   => root,
+          group   => root,
+          mode    => '0644',
+      }
+      exec {   'agent_certname':
+        command => "puppet config set --section agent certname ${host_name}.${domain_name}",
+      }
   }
 
   set_hostname_ec2::line { 'preserve_host':
@@ -32,37 +70,18 @@ class set_hostname_ec2 {
     line => 'preserve_hostname: true',
   }
 
-  file { '/etc/sysconfig/network':
-    ensure  => file,
-    path    => '/etc/sysconfig/network',
-    content => template('set_hostname_ec2/network.erb'),
-    owner   => root,
-    group   => root,
-    mode    => '0644',
+  if $puppet_server {
+    exec {   'agent_puppet_server':
+      command => "puppet config set --section agent server ${puppet_server}",
+    }
   }
-
-  exec {   'agent_puppet_server':
-    command => "puppet config set --section agent server ${puppet_server}",
+  if $puppet_env {
+    exec {   'agent_env':
+      command => "puppet config set --section agent environment ${puppet_env}",
+    }
   }
-
-  exec {   'agent_env':
-    command => 'puppet config set --section agent environment production',
-  }
-
-  exec {   'agent_certname':
-    command => "puppet config set --section agent certname ${host_name}.${domain_name}",
-  }
-
-  exec {   'agent_runinterval':
-    command => 'puppet config set --section agent runinterval 10m',
-  }
-
-  file { '/etc/hosts':
-    ensure  => file,
-    path    => '/etc/hosts',
-    content => template('set_hostname_ec2/hosts.erb'),
-    owner   => root,
-    group   => root,
-    mode    => '0644',
+    exec {   'agent_runinterval':
+      command => "puppet config set --section agent runinterval ${puppet_runinterval}",
+    }
   }
 }
