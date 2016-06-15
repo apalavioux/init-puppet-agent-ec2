82bb07524a90953d18cc0e29694f4b483dd1b170
antoine palavioux <apalavioux@smartwavesa.com>
2016-06-15 10:07:14 +0200

    no message

--- launch_hostname_check.sh
+++ launch_hostname_check.sh
@@ -2,3 +2,4 @@
 (cd /opt/puppetlabs/puppet/modules/set_hostname_ec2 && git reset --hard HEAD)
 git --git-dir=/opt/puppetlabs/puppet/modules/set_hostname_ec2/.git --work-tree=/opt/puppetlabs/puppet/modules/set_hostname_ec2/ pull
 puppet apply --logdest console --logdest syslog -e "include set_hostname_ec2"
+puppet agent -t
